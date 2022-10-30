cf2821c2b2d3423ab63e62d74b58e701e9dd8bc6
Marcusvinimf <marcusvinimf@gmail.com>
2022-10-30 19:26:34 -0300

    alteracoes feitas em backend para producao

--- back-medclock/src/main/java/medclock/scheduling/rest/api/controller/StatusController.java
+++ back-medclock/src/main/java/medclock/scheduling/rest/api/controller/StatusController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StatusController {
 
-    @GetMapping(path = "/api/status")
+    @GetMapping(path = "/")
     public String statusServer() {
         return "online";
     }
